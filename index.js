10135f4c43cbe72d8c7c5360a2fc8b7127392df6
Franco Negrete <franconegrete12@gmail.com>
2021-11-18 18:29:27 -0300

    Desafio v-1.0.0

---
+++ index.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Cual es tu nombre?");
+alert("Woow, que bello nombre.");
+let edad = prompt("Y tu edad?");
+alert("Que genial, y si te digo que puedo adivinar el año en el que naciste...");
+let year = 2021;
+let resultado = 0;
+resultado = year - edad;
+alert("El año en el que naciste es.." + resultado);
