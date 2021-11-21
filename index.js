f04194c8bad9b240e7b483c9466c3cfe506d3c52
Franco Negrete <franconegrete12@gmail.com>
2021-11-21 02:23:23 -0300

    V-1.1.0 Desafio 2

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-let nombre = prompt("Cual es tu nombre?");
+//Desafio Uno
+
+
+/*let nombre = prompt("Cual es tu nombre?");
 alert("Woow, que bello nombre.");
 let edad = prompt("Y tu edad?");
 alert("Que genial, y si te digo que puedo adivinar el año en el que naciste...");
@@ -6,3 +9,21 @@ let year = 2021;
 let resultado = 0;
 resultado = year - edad;
 alert("El año en el que naciste es.." + resultado);
+*/
+
+//desafio Dos
+
+let nombre = prompt("Ingresa tu nombre");
+if (nombre != ""){
+    alert("Hola "+ nombre);
+}else if (nombre === ""){
+    alert("No ha ingresado ningun nombre.")
+}
+let edad = prompt("Ingresa tu edad");
+if (edad <= 18){
+    alert("Woow, que joven eres!")
+}else if (edad > 18){
+    alert("estas en tus años dorados, aprovechalos!")
+}
+
+
