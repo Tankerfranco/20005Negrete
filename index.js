3ae8110b9a0031a279452379af49f7b32ed9b03b
Franco Negrete <franconegrete12@gmail.com>
2021-11-22 12:43:57 -0300

    v-1.2.1 cambios en el lugar de la variable

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 //Desafio Uno
 
-
-/*let nombre = prompt("Cual es tu nombre?");
-alert("Woow, que bello nombre.");
-let edad = prompt("Y tu edad?");
-alert("Que genial, y si te digo que puedo adivinar el año en el que naciste...");
-let year = 2021;
-let resultado = 0;
-resultado = year - edad;
-alert("El año en el que naciste es.." + resultado);
-*/
+// let nombre;
+// let year = 2021;
+// let resultado = 0;
+// let edad = 0;
+// nombre = prompt("Cual es tu nombre?");
+// alert("Woow, que bello nombre.");
+// edad = prompt("Y tu edad?");
+// alert("Que genial, y si te digo que puedo adivinar el año en el que naciste...");
+// resultado = year - edad;
+// alert("El año en el que naciste es.." + resultado);
 
 //desafio Dos
 
-let nombre = prompt("Ingresa tu nombre");
+let nombre;
+let edad = 0;
+nombre = prompt("Ingresa tu nombre");
 if (nombre != ""){
     alert("Hola "+ nombre);
 }else if (nombre === ""){
     alert("No ha ingresado ningun nombre.")
 }
-let edad = prompt("Ingresa tu edad");
+edad = prompt("Ingresa tu edad");
 if (edad <= 18){
     alert("Woow, que joven eres!")
 }else if (edad > 18){
     alert("estas en tus años dorados, aprovechalos!")
 }
-
-
