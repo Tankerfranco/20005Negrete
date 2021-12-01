aa4fb2d93b6af143eba3563e5e5945586e450616
Franco Negrete <franconegrete12@gmail.com>
2021-12-01 20:20:53 -0300

    Desafio 4

--- README.md
+++ README.md
@@ -7,4 +7,7 @@
  *  - v1.0.1 – Primera entrega.
  *  - v1.1.0 – Segunda entrega.
  *  - v1.2.0 – Tercera entrega.
+ *  - v1.2.1 – cambios en el desafio 3.
+ *  - v1.3.0 – Cuarta entrega.
+ *  - v1.4.0 – Primera entrega complementaria.
  */
--- index.js
+++ index.js
@@ -30,12 +30,12 @@ if (edad <= 18){
 
 // Desafio Tres
 
-let numUno, numDos, operacion, salida;
+/* let numUno, numDos, operacion, salida;
 
 operacion = prompt("Que operacion quieres hacer?(+, -, *, /)");
 alert("ahora te pedire dos numeros");
-numUno = parseInt(prompt("Ingresa un numero: "));
-numDos = parseInt(prompt("Ingrese un numero: "));
+numUno = parseFloat(prompt("Ingresa un numero: "));
+numDos = parseFloat(prompt("Ingrese un numero: "));
 while(salida != "Salir"){
     switch(operacion){
         case "+":
@@ -59,9 +59,92 @@ while(salida != "Salir"){
     if(salida == ""){
         operacion = prompt("Que operacion quieres hacer?(+, -, *, /)");
         alert("ahora te pedire dos numeros");
-        numUno = parseInt(prompt("Ingresa un numero: "));
-        numDos = parseInt(prompt("Ingrese un numero: "));
+        numUno = parseFloat(prompt("Ingresa un numero: "));
+        numDos = parseFloat(prompt("Ingrese un numero: "));
+    }
+} */
+
+//Desafio Cuatro
+
+/* let compras, salida, recibo;
+
+function saludo(){ //Esta funcion te saluda cuando ingresas a la pagina.
+    return alert("Bienvenido a la Tierra Bendita");
+}
+
+function pedido(){ //Esta funcion es para ver que pedido quiere el usuario.
+    while (salida != "Esc"){
+        switch(compras){
+            case "Milanesas":
+                alert("Ahora le traemos sus milanesas.");
+                break;
+            case "Hamburguesas":
+                alert("Ahora le traemos su hamburguesas");
+                break;
+            case "Helado":
+                alert("Ahora le traemos su helado");
+                break;    
+            default:
+                alert("Disculpe eso no esta en el menu");
+                break;
+        }
+        salida = prompt("Quiere pedir algo mas?");
+        if (salida === "si") {
+            compras = prompt("que mas quiere comer? (Milanesas, Hamburguesas o Helado)");
+        }
+        else {
+            salida = prompt("Sino desea nada mas.. escriba 'Esc'");
+        }
     }
 }
 
+function cuenta(){ //Esta funcion es para ver cuanto tiene que pagar el usuario.
+    recibo = prompt("Disculpe que lo moleste, pero nuestro mesero es nuevo y olvido que pidio usted, seria tan amable de decirme que pidio?(Milanesas, Helado o Hamburguesas)");
+    switch(recibo){
+        case "Milanesas":
+            alert("Esta bien, serian 200 pesos.");
+            break;
+        case "hamburguesas":
+            alert("Esta bien, serian 300 pesos.");
+            break;
+        case "Helado":
+            alert("Esta bien, serian 150 pesos.");
+            break;
+        default:
+            alert("Disculpe, podria repetirlo.");
+            recibo = prompt("seria tan amable de decirme que pidio?(Milaneas, Helado o Hamburguesas)");
+            break;
+    }
+}
 
+saludo();
+compras = prompt("Que desea comprar? (Milanesas, Hamburguesas o Helado)");
+pedido();
+recibo = prompt("Espero alla disfrutado de su comida, ahora le traigo la cuenta.");
+cuenta(); 
+   */
+
+//Desafio Complementario
+
+/* let nombre, direccion, edad;
+
+function name(){
+    nombre = prompt("Cual es tu nombre? ");
+    return nombre;
+}
+
+function adress(){
+    direccion = prompt("Donde vives? ");
+    return direccion;
+}
+
+function age(){
+    edad = prompt("Cual es tu edad? ");
+    return edad;
+}
+
+alert("Hola, necesito los siguientes datos para tu carnet.");
+name();
+adress();
+age();
+alert("Tus datos son "+ nombre +" " + direccion +" " + edad); */
