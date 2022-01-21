aee277f15cab62c6d73857cfc77f2da9e95c36fa
Franco Negrete <franconegrete12@gmail.com>
2022-01-20 21:57:41 -0300

    desafio ajax

--- desafios/ejemplo.js
+++ desafios/ejemplo.js
@@ -82,7 +82,7 @@ const cards = () => {
  */
 
 //Desafio Once y complementario.
-
+/* 
 $("h1").mouseenter(function(){
     $("#primero").fadeOut();
 })
@@ -123,3 +123,70 @@ $(".doy").click(function(){
 $(".quito").click(function(){
     $("h1").removeClass("blue")
 })
+ */
+
+//Desafio Ajax
+let boton = document.getElementById("pagar")
+let botonMostrar = document.getElementById("mostrar")
+let text = document.getElementById("text")
+
+$("#pagar").click(function(){
+    pagar();
+})
+
+$("#mostrar").click(function(){
+    traerDatos(text.value);
+})
+
+/* botonMostrar.addEventListener("click", (e) => {
+traerDatos(text.value)
+}) */
+
+async function traerDatos (product) {
+    console.log(product)
+    let response = await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${product}`)
+    let data = await response.json()
+    console.log(response)
+    console.error(data)
+}
+
+const carrito = [
+    {nombre: "Auricular", descripcion: "otra cosa", img: "imagen", id: 1, cantidad: 2, precio: 100},
+    {nombre: "Auricular", descripcion: "otra cosa", img: "imagen", id: 2, cantidad: 1, precio: 1400},
+    {nombre: "Auricular", descripcion: "otra cosa", img: "imagen", id: 2, cantidad: 3, precio: 100},
+    {nombre: "Auricular", descripcion: "otra cosa", img: "imagen", id: 2, cantidad: 4, precio: 100}]
+
+
+
+async function pagar() {
+    const productsToMP = carrito.map((element) => {
+      let nuevoElemento = {
+        title: element.nombre,
+        description: element.descripcion,
+        picture_url: element.img,
+        category_id: element.id,
+        quantity: Number(element.cantidad),
+        currency_id: "ARS",
+        unit_price: Number(element.precio),
+      };
+      return nuevoElemento;
+    });
+    console.log(productsToMP)
+    const response = await fetch(
+      "https://api.mercadopago.com/checkout/preferences",
+      {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Bearer TEST-680675151110839-052307-64069089337ab3707ea2f547622a1b6a-60191006",
+        },
+        body: JSON.stringify({
+          items: productsToMP,
+        }),
+      }
+    );
+    console.warn(response)
+    const data = await response.json();
+    console.log(data)
+    window.open(data.init_point, "_blank");
+  }
